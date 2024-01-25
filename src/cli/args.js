4bfd20fde0588e4871785a83b8f2fd7a6609833c
AliaksandrChachura <chatliany198560@gmail.com>
2024-01-25 13:35:17 +0100

    create: cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    if (args.length % 2 !== 0) {
+        console.error('Invalid number of command line arguments. Each property must have a corresponding value.');
+        return;
+    }
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace(/^--/, '');
+        const propValue = args[i + 1];
+
+        console.log(`${propName} is ${propValue}`);
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssVariables = {};
+    
+    for (const key in process.env) {
+        if (key.startsWith("RSS_")) {
+            rssVariables[key] = process.env[key];
+        }
+    }
+
+    if (Object.keys(rssVariables).length > 0) {
+        const formattedOutput = Object.entries(rssVariables).map(([key, value]) => `RSS_${key}=${value}`).join('; ');
+        console.log(formattedOutput);
+    } else {
+        console.log("No environment variables with the prefix 'RSS_' found.");
+    }
 };
 
 parseEnv();
