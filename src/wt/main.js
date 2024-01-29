1cc799b7aa2b6039344aa85a3799f9432f704174
AliaksandrChachura <chatliany198560@gmail.com>
2024-01-29 22:58:19 +0100

    modify: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+import { cpus } from 'node:os';
+
+const createWorker = (data) => {
+  return new Promise((resolve) => {
+    const worker = new Worker('./wt/worker.js', { workerData: data });
+    worker.on('message', (result) => {
+      resolve(result);
+    });
+  });
+};
+
+const createWorkers = async (numWorkers) => {
+  const workers = [];
+  for (let i = 0; i < numWorkers; i++) {
+    const data = 10 + i;
+    const result = await createWorker(data);
+    workers.push(result);
+  }
+  return workers;
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        const numCores = cpus().length;
+        const workers = await createWorkers(numCores);
+    
+        console.log(workers);
+    }
 };
 
 await performCalculations();
