6091054d5d692b2f56fba45bf073083d47851ab2
AliaksandrChachura <chatliany198560@gmail.com>
2024-03-14 20:20:06 +0100

    feat: refactor create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,32 +1,22 @@
 import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const create = async () => {
-	const __filename = fileURLToPath(import.meta.url);
-	const __dirname = path.dirname(__filename);
-
-	const folderPath = path.join(__dirname, 'files');
-	const filePath = path.join(folderPath, 'fresh.txt');
-	const fileContent = 'I am fresh and young';
 
 	try {
-		await fs.access(filePath);
-		throw new Error('FS operation failed');
-	} catch (error) {
-		if (error.code === 'ENOENT') {
-			try {
-					await fs.mkdir(folderPath, { recursive: true });
-
-					await fs.writeFile(filePath, fileContent);
-
-					console.log('File created successfully:', filePath);
-			} catch (error) {
-				throw new Error('FS operation failed');
-			}
-	} else {
-		throw new Error('FS operation failed');
-	}
+		await fs.appendFile(
+				resolve(__dirname, "files", "fresh.txt"),
+				"I am fresh and young",
+				{
+						flag: "ax"
+				}
+		);
+		console.log("File successfully created");
+	} catch (e) {
+			throw new Error("FS operation failed");
 	}
 };
 
