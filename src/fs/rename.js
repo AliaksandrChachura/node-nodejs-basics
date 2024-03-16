e9a9ba352c04926074c8d81ed4a24e12d3f5aeb5
AliaksandrChachura <chatliany198560@gmail.com>
2024-03-16 17:15:36 +0100

    feat: refactor rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,36 +1,29 @@
-import fs from 'node:fs/promises';
-import path from 'path';
+import * as fs from 'node:fs/promises';
 import { fileURLToPath } from 'url';
+import {dirname, resolve} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const sourceFolder = 'files';
+const sourceFile = 'wrongFilename.txt';
+const destinationFile = 'properFilename.md';
 
 const rename = async () => {
-    const sourceFolder = 'files';
-    const sourceFile = 'wrongFilename.txt';
-    const destinationFile = 'properFilename.md';
-
-    const __filename = fileURLToPath(import.meta.url);
-	const __dirname = path.dirname(__filename);
-
-    const wrongFilePath = path.join(__dirname, sourceFolder, sourceFile);
-    const properFilePath = path.join(__dirname, sourceFolder, destinationFile); 
-
     try {
-        await fs.access(wrongFilePath);
-        try {
-            await fs.access(properFilePath, fs.constants.F_OK);
-            throw new Error('FS operation failed');
-        } catch (error) {
-            if (error.code === 'ENOENT') {
-                await fs.rename(wrongFilePath, properFilePath);
-            } else {
-                throw new Error(`FS operation failed`);
-            }
+        const data = await fs.readdir(resolve(__dirname, sourceFolder));
+
+        if (data.includes(destinationFile)) {
+            throw new Error("FS operation failed");
         }
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            throw new Error('FS operation failed');
-        } else {
-            throw new Error('FS operation failed');
-        }
+
+        await fs.rename(
+            resolve(__dirname, sourceFolder, sourceFile),
+            resolve(__dirname, sourceFolder, destinationFile)
+        );
+
+        console.log("File successfully renamed");
+    } catch (e) {
+        throw new Error("FS operation failed");
     }
 };
 
