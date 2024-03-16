c81ec2457b8dfd3a1d22af02c4d92196796bbe15
AliaksandrChachura <chatliany198560@gmail.com>
2024-03-16 17:24:58 +0100

    feat: refactor delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,25 +1,20 @@
-import fs from 'node:fs/promises';
-import path from 'path';
+import * as fs from 'node:fs/promises';
+import { dirname, resolve} from 'path';
 import { fileURLToPath } from 'url';
 
+const sourceFolder = 'files';
+const sourceFile = 'fileToRemove.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    const sourceFolder = 'files';
-    const sourceFile = 'fileToRemove.txt';
-
-    const __filename = fileURLToPath(import.meta.url);
-	const __dirname = path.dirname(__filename);
-
-    const removeFilePath = path.join(__dirname, sourceFolder, sourceFile);
+    const removeFilePath = resolve(__dirname, sourceFolder, sourceFile);
 
     try {
-        await fs.access(removeFilePath);
         await fs.rm(removeFilePath);
     } catch (error) {
-        if (error.code === 'ENOENT') {
-            throw new Error(`FS operation failed`);
-        } else {
-            throw new Error(`FS operation failed`);
-        }
+        throw new Error(`FS operation failed`);
     }
 };
 
