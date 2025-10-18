b534d76d88267239afa8bb8eddd278cc7d7be302
AliaksandrChachura <47753412+AliaksandrChachura@users.noreply.github.com>
2025-10-18 22:43:15 +0200

    fix: update copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,25 +1,25 @@
-import * as fs from 'node:fs/promises';
-import {dirname, resolve} from "node:path";
+import { readdir, mkdir, copyFile, constants} from 'node:fs/promises';
+import { dirname, resolve } from "node:path";
 import { fileURLToPath } from 'node:url';
 
 const copy = async () => {
     const __filename = fileURLToPath(import.meta.url);
-	const __dirname = dirname(__filename);
+	  const __dirname = dirname(__filename);
 
     const sourceFolder = 'files';
     const destinationFolder = 'files_copy';
 
     try {
-        const files = await fs.readdir(resolve(__dirname, sourceFolder));
-        await fs.mkdir(resolve(__dirname, destinationFolder), {
+        const files = await readdir(resolve(__dirname, sourceFolder));
+        await mkdir(resolve(__dirname, destinationFolder), {
           recursive: false
         });
     
         const copyPromises = files.map(file =>
-          fs.copyFile(
+          copyFile(
             resolve(__dirname, "files", `${file}`),
             resolve(__dirname, "files_copy", `${file}`),
-            fs.constants.COPYFILE_EXCL
+            constants.COPYFILE_EXCL
           )
         );
     
