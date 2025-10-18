98deed98c2d1e0dd370141f1399599918b992753
AliaksandrChachura <47753412+AliaksandrChachura@users.noreply.github.com>
2025-10-19 00:02:15 +0200

    fix: update parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,15 +1,19 @@
 const parseEnv = () => {
-    const formattedOutput = Object.entries(process.env)
-        .filter(item => item[0]
-        .startsWith("RSS_"))
-        .map(([key, value]) => `RSS_${key}=${value}`)
+    const rssEntries = Object.entries(process.env)
+        .filter(item => item[0].startsWith("RSS_"));
+        
+
+    if (rssEntries.length < 1) {
+        console.log("No environment variables with the prefix 'RSS_' found.");
+    }
+
+    const formattedOutput = rssEntries 
+        .map(([key, value]) => `${key}=${value}`)
         .join('; ');
 
     process.stdout.write(formattedOutput);
 
-    if (formattedOutput.length < 1) {
-        console.log("No environment variables with the prefix 'RSS_' found.");
-    }
+    
 };
 
 parseEnv();
