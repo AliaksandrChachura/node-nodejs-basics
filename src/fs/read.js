95865dad5181693fcdc23c44a6ae7d760fd23452
AliaksandrChachura <47753412+AliaksandrChachura@users.noreply.github.com>
2025-10-18 23:40:01 +0200

    fix: update read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,26 +1,27 @@
-import * as fs from 'node:fs/promises';
+import { access, readFile as readFileFs } from 'node:fs/promises';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const readFilePath = resolve(__dirname, sourceFolder, readFile);
-
 const sourceFolder = 'files';
 const readFile = 'fileToRead.txt';
+
+const readFilePath = resolve(__dirname, sourceFolder, readFile);
+
 
 const read = async () => {
     
 
     try {
-        await fs.access(readFilePath);
-        const contents = await fs.readFile(
+        await access(readFilePath);
+        const contents = await readFileFs(
             readFilePath,
             { encoding: 'utf8' }
         );
         console.log(contents);
-    } catch (error) {
+    } catch {
         throw new Error(`FS operation failed`);
     }
 };
