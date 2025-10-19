718c4bb00c0b95cd0c267c784a5fc1fbd96dc7aa
AliaksandrChachura <47753412+AliaksandrChachura@users.noreply.github.com>
2025-10-19 23:07:56 +0200

    fix: update spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -9,12 +9,26 @@ const __dirname = dirname(__filename);
 const spawnChildProcess = async (args) => {
     const child = fork(resolve(__dirname, "files", "script.js"), [
         ...args.slice(2)
-      ]);
-      console.log(process.pid);
-      child.on("message", (message) => {
+    ], {
+        silent: true
+    });
+    
+    console.log(`Master process PID: ${process.pid}`);
+    console.log(`Child process PID: ${child.pid}`);
+    
+    process.stdin.pipe(child.stdin);
+    
+    child.stdout.pipe(process.stdout);
+    
+    child.stderr.pipe(process.stderr);
+    
+    child.on("message", (message) => {
         console.log("Message from child:", message.toString());
-      });
+    });
+    
+    child.on("exit", (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess( process.argv);
