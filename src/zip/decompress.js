3c6db461888acec842e15f49b347a35596cba83b
AliaksandrChachura <47753412+AliaksandrChachura@users.noreply.github.com>
2025-10-19 14:00:24 +0200

    fix: update decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -4,24 +4,38 @@ import{
 } from "fs";
 import { createGunzip } from 'node:zlib';
 import { rm } from "fs/promises";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const sourceFolder = 'files';
+const folderPath = resolve(__dirname, sourceFolder);
 
 const decompress = async () => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    const sourceFolder = 'files';
-    const folderPath = resolve(__dirname, sourceFolder);
+    const source = resolve(folderPath, "archive.gz");
+    const destination = resolve(folderPath, "fileToCompress.txt");
+    
+    try {
+        const pipelineAsync = promisify(pipeline);
+        await pipelineAsync(
+            createReadStream(source),
+            createGunzip(),
+            createWriteStream(destination)
+        );
 
-    createReadStream(resolve(folderPath, "archive.gz"))
-        .pipe(createGunzip())
-        .pipe(createWriteStream(resolve(folderPath, "fileToCompress.txt")))
-        .on("finish", async () => {
-            await rm(resolve(folderPath, `archive.gz`));
-            console.log("Decompression done!");
-    });
+        await rm(source);
+    } catch (err) {
+        if (err.code === "EEXIST") {
+            console.error('The file already exists');
+        } else if (err.code === "ENOENT") {
+            console.error('The file does not exist');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await decompress();
